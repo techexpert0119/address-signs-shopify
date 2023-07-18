2ac80d7edb0a63c44af8ddd166f0fd5894a0b211
TechExpert0119 <techexpert0119@gmail.com>
2023-07-18 15:33:07 -0700

    Background change on click functionality

--- assets/create-sign-product.js
+++ assets/create-sign-product.js
@@ -1,73 +1,4 @@
-const LetterWidths = {
-    "\n": 0,
-    "0": 6.1,
-    "1": 5.2,
-    "2": 7,
-    "3": 6.7,
-    "4": 6.7,
-    "5": 6.5,
-    "6": 5.7,
-    "7": 6.3,
-    "8": 6,
-    "9": 5.7,
-    ",": 2.1,
-    ".": 1.4,
-    "A": 11.7,
-    "a": 6.2,
-    "'": 2.1,
-    "B": 11.1,
-    "b": 4.6,
-    "C": 7.4,
-    "c": 4.3,
-    "D": 10.7,
-    "d": 7.4,
-    "-": 3.5,
-    "E": 8,
-    "e": 4.5,
-    "F": 11.1,
-    "f": 11,
-    "G": 10,
-    "g": 7.3,
-    "H": 14.4,
-    "h": 6.7,
-    "I": 10,
-    "i": 4,
-    "J": 11.7,
-    "j": 8.5,
-    "K": 12.7,
-    "k": 6.8,
-    "L": 10.1,
-    "l": 5,
-    "M": 14.1,
-    "m": 9.5,
-    "N": 13.5,
-    "n": 6.8,
-    "O": 7.4,
-    "o": 4.6,
-    "P": 10.7,
-    "p": 7.4,
-    "Q": 8.5,
-    "q": 5.6,
-    "R": 11,
-    "r": 3.4,
-    "S": 8,
-    "s": 3.4,
-    " ": 5,
-    "T": 11.2,
-    "t": 4.4,
-    "U": 10,
-    "u": 6.2,
-    "V": 8.7,
-    "v": 4.4,
-    "W": 10.5,
-    "w": 7,
-    "X": 11.3,
-    "x": 5.8,
-    "Y": 11,
-    "y": 7.4,
-    "Z": 11.3,
-    "z": 5.6
-  };
+const LetterWidths = {"\n": 0,"0": 6.1,"1": 5.2,"2": 7,"3": 6.7,"4": 6.7,"5": 6.5,"6": 5.7,"7": 6.3,"8": 6,"9": 5.7,",": 2.1,".": 1.4,"A": 11.7,"a": 6.2,"'": 2.1,"B": 11.1,"b": 4.6,"C": 7.4,"c": 4.3,"D": 10.7,"d": 7.4,"-": 3.5,"E": 8,"e": 4.5,"F": 11.1,"f": 11,"G": 10,"g": 7.3,"H": 14.4,"h": 6.7,"I": 10,"i": 4,"J": 11.7,"j": 8.5,"K": 12.7,"k": 6.8,"L": 10.1,"l": 5,"M": 14.1,"m": 9.5,"N": 13.5,"n": 6.8,"O": 7.4,"o": 4.6,"P": 10.7,"p": 7.4,"Q": 8.5,"q": 5.6,"R": 11,"r": 3.4,"S": 8,"s": 3.4," ": 5,"T": 11.2,"t": 4.4,"U": 10,"u": 6.2,"V": 8.7,"v": 4.4,"W": 10.5,"w": 7,"X": 11.3,"x": 5.8,"Y": 11,"y": 7.4,"Z": 11.3,"z": 5.6};
 
 // Function to calculate the total value of a string
 function calculateValue(str) {
@@ -106,3 +37,14 @@ function filterInput() {
 // Add event listeners to the text input
 document.getElementById('textInput').addEventListener('input', filterInput);
 document.getElementById('textInput').addEventListener('keyup', countValue);
+
+// BACKGROUND CHANGE ON CLICK FUNCTIONALITY
+const backgroundSwatches = document.querySelectorAll('.csp-swatch-box');
+const currentBackgroundImage = document.querySelector('#csp-material-background');
+console.log(currentBackgroundImage);
+
+backgroundSwatches.forEach( swatch => {
+    swatch.addEventListener('click', function(e) {
+        currentBackgroundImage.src = e.currentTarget.querySelector('.csp-swatch-img').src;
+    });
+});
